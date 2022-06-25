bd11000033950222f621d5a86fd4cabef6346b57
MisaLiu <misaliu@misaliu.top>
2022-06-25 19:26:34 +0800

    Project init

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     devServer: {
         static: {
-        directory: path.join(__dirname, 'public'),
+            directory: path.join(__dirname, 'public'),
         },
         compress: true,
         port: 9000,
