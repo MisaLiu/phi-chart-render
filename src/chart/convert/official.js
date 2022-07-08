8470176d958fa0f931bbd36cbc7267a980ff8591
MisaLiu <misaliu@misaliu.top>
2022-07-08 16:51:26 +0800

    怎么缩进一会是tab一会是空格

--- src/chart/convert/official.js
+++ src/chart/convert/official.js
@@ -114,52 +114,52 @@ export default function OfficialChartConverter(_chart)
 function convertOfficialVersion(chart)
 {
     let newChart = JSON.parse(JSON.stringify(chart));
-	
-	switch (newChart.formatVersion)
+    
+    switch (newChart.formatVersion)
     {
-		case 1:
+        case 1:
         {
-			newChart.formatVersion = 3;
-			for (const i of newChart.judgeLineList)
+            newChart.formatVersion = 3;
+            for (const i of newChart.judgeLineList)
             {
-				let floorPosition = 0;
-				
-				for (const x of i.speedEvents)
+                let floorPosition = 0;
+                
+                for (const x of i.speedEvents)
                 {
-					if (x.startTime < 0) x.startTime = 0;
-					x.floorPosition = floorPosition;
-					floorPosition += (x.endTime - x.startTime) * x.value / i.bpm * 1.875;
-				}
-				
-				for (const x of i.judgeLineDisappearEvents)
+                    if (x.startTime < 0) x.startTime = 0;
+                    x.floorPosition = floorPosition;
+                    floorPosition += (x.endTime - x.startTime) * x.value / i.bpm * 1.875;
+                }
+                
+                for (const x of i.judgeLineDisappearEvents)
                 {
-					x.start2 = 0;
-					x.end2   = 0;
-				}
-				
-				for (const x of i.judgeLineMoveEvents)
+                    x.start2 = 0;
+                    x.end2   = 0;
+                }
+                
+                for (const x of i.judgeLineMoveEvents)
                 {
-					x.start2 = x.start % 1e3 / 520;
-					x.end2   = x.end % 1e3 / 520;
-					x.start  = parseInt(x.start / 1e3) / 880;
-					x.end    = parseInt(x.end / 1e3) / 880;
-				}
-				
-				for (const x of i.judgeLineRotateEvents)
+                    x.start2 = x.start % 1e3 / 520;
+                    x.end2   = x.end % 1e3 / 520;
+                    x.start  = parseInt(x.start / 1e3) / 880;
+                    x.end    = parseInt(x.end / 1e3) / 880;
+                }
+                
+                for (const x of i.judgeLineRotateEvents)
                 {
-					x.start2 = 0;
-					x.end2   = 0;
-				}
-			}
-		}
-		case 3: {
+                    x.start2 = 0;
+                    x.end2   = 0;
+                }
+            }
+        }
+        case 3: {
             break;
         }
-		default:
-			throw new Error('Unsupported chart version: ' + newChart.formatVersion);
-	}
-	
-	return newChart;
+        default:
+            throw new Error('Unsupported chart version: ' + newChart.formatVersion);
+    }
+    
+    return newChart;
 }
 
 function calcRealTime(time, bpm) {
