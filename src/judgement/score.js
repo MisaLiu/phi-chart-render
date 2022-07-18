3ca07556095b88b306cd744bef89ff650c02124a
MisaLiu <misaliu@misaliu.top>
2022-07-19 00:00:39 +0800

    开始写分数系统

--- src/judgement/score.js
+++ src/judgement/score.js
@@ -1,4 +1,18 @@
 export default class Score
 {
-    
+    constructor(notesCount, isChallengeMode = false)
+    {
+        this.scorePerNote = isChallengeMode ? 1000000 / notesCount : 900000 / notesCount;
+        this.scorePerCombo = isChallengeMode ? 0 : 100000 / notesCount;
+        
+        this.score    = 0;
+        this.acc      = 0;
+        this.combo    = 0;
+        this.maxCombo = 0;
+
+        this.perfect = 0;
+        this.good    = 0;
+        this.bad     = 0;
+        this.miss    = 0;
+    }
 }
