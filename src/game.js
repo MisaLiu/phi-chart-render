56c67b5443cde5f943a055a995cfa543c8f80189
MisaLiu <misaliu@misaliu.top>
2022-10-01 03:36:08 +0800

    避免打击动画报错的临时解法

--- src/game.js
+++ src/game.js
@@ -65,7 +65,7 @@ export default class Game
             chart    : this.chart,
             stage    : this.render.mainContainer,
             canvas   : this.render.view,
-            textures : { normal: this.assets.textures.clickRaw, bad: [] },
+            textures : { normal: this.assets.textures.clickRaw, bad: this.assets.textures.clickRaw },
             sounds   : this.assets.sounds,
             autoPlay : params.settings && params.settings.autoPlay ? !!params.settings.autoPlay : false
         });
