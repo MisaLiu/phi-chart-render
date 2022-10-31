da422e59c93d76b3fa0043a34176584c91ee3ea3
MisaLiu <misaliu@misaliu.top>
2022-10-31 19:25:44 +0800

    修改 sentry 的 sourcemap 上传方式

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,13 @@
  * @Author: git config user.name && git config user.email
  * @Date: 2022-07-17 20:27:06
  * @LastEditors: git config user.name && git config user.email
- * @LastEditTime: 2022-10-31 19:01:59
+ * @LastEditTime: 2022-10-31 19:23:29
  * @FilePath: \phi-chart-render\webpack.config.js
  * @Description: 
  * 
  * Copyright (c) 2022 by ${git_name_email}, All Rights Reserved. 
  */
 const path = require('path');
-const SentryPlugin = require('@sentry/webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -34,13 +33,5 @@ module.exports = {
         compress: true,
         port: 9000,
         open: true
-    },
-    plugins: [
-        new SentryPlugin({
-            release: true,
-            include: './dist',
-            ignoreFile: '.gitignore',
-            ignore: [ 'node_modules', 'webpack.config.js' ]
-        })
-    ]
+    }
 };
