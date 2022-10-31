c9ccf737cead926ce8bdf5a89c5b63241164a7db
MisaLiu <misaliu@misaliu.top>
2022-10-31 19:04:50 +0800

    配置 sentry-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,19 @@
  * @Author: git config user.name && git config user.email
  * @Date: 2022-07-17 20:27:06
  * @LastEditors: git config user.name && git config user.email
- * @LastEditTime: 2022-10-12 20:53:35
+ * @LastEditTime: 2022-10-31 19:01:59
  * @FilePath: \phi-chart-render\webpack.config.js
  * @Description: 
  * 
  * Copyright (c) 2022 by ${git_name_email}, All Rights Reserved. 
  */
 const path = require('path');
+const SentryPlugin = require('@sentry/webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: '/src/index.js',
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'script.js'
@@ -33,4 +35,12 @@ module.exports = {
         port: 9000,
         open: true
     },
+    plugins: [
+        new SentryPlugin({
+            release: true,
+            include: './dist',
+            ignoreFile: '.gitignore',
+            ignore: [ 'node_modules', 'webpack.config.js' ]
+        })
+    ]
 };
