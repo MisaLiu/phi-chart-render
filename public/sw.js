b597755dc40dd88b182f8ded9e1cce30fd0f1e77
MisaLiu <misaliu@misaliu.top>
2023-02-28 03:49:12 +0800

    更新 sw.js 以适配新的打包流程

--- public/sw.js
+++ public/sw.js
@@ -44,16 +44,16 @@ self.addEventListener('activate', (e) =>
 self.addEventListener('fetch', (e) =>
 {
     if (e.request.method != 'GET') return;
-    if (!(/phi-chart-render\/?$|index\.html|\.css|script\.js\?hash=|assets|fonts/.test(e.request.url))) return;
+    if (!(/phi-chart-render\/?$|index\.html|\.css|script\-[v0-9a-z\-\.]+\.js|assets|fonts/.test(e.request.url))) return;
 
     let req = e.request;
-    let resVersion = (/phi-chart-render\/?$|index\.html|\.css|script\.js\?hash=/.test(e.request.url)) ? GITHUB_CURRENT_GIT_HASH : ASSETS_VERSION;
+    let resVersion = (/phi-chart-render\/?$|index\.html|\.css|script\-[v0-9a-z\-\.]+\.js/.test(e.request.url)) ? GITHUB_CURRENT_GIT_HASH : ASSETS_VERSION;
 
     e.respondWith(
         caches.match(e.request)
             .then((res) =>
             {
-                if (res && !(/phi-chart-render\/?$|index\.html|\.css|script\.js\?hash=/.test(e.request.url)))
+                if (res && !(/phi-chart-render\/?$|index\.html|\.css|script\-[v0-9a-z\-\.]+\.js/.test(e.request.url)))
                 {
                     console.log('[Service Worker] Fetching cache: ' + req.url);
                     return res;
