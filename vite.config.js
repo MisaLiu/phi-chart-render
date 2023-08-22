4a2796fb7804bb2563d9e5637e5c88c2961331b5
MisaLiu <misaliu@misaliu.top>
2023-08-22 19:42:53 +0800

    修改 pwa 实现方法

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import config from './package.json';
 import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import { VitePWA } from 'vite-plugin-pwa';
 import git from 'git-rev-sync';
 import path from 'path';
 
 const CurrentVersion = 'v' + config.version + '-' + git.short();
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +16,46 @@ export default defineConfig({
         data: {
           GIT_VERSION: CurrentVersion
         }
+      }
+    }),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      workbox: {
+        cleanupOutdatedCaches: true,
+        runtimeCaching: [
+          {
+            urlPattern: /\/phi-chart-render(.*?)\.(png|ico|ogg)/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'assets-cache',
+            },
+          }
+        ],
+      },
+      minify: true,
+      manifest: {
+        id: 'misaliu-phi-chart-render',
+        name: 'phi-chart-render',
+        short_name: 'phi-chart-render',
+        description: 'A Phigros chart render based on Pixi.js',
+        scope: '/phi-chart-render/',
+        display: 'standalone',
+        orientation: 'landscape',
+        background_color: '#000000',
+        includeAssets: [ './icons/favicon.ico' ],
+        icons: [
+          {
+            src: './icons/64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: './icons/192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
       }
     }),
   ],
@@ -31,6 +71,9 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 9000,
     open: true
+  },
+  build: {
+    sourcemap: true
   }
 });
 
