f1f874c48c472c88f6617d1c076e989ed170c22e
IcedDog <34804287+IcedDog@users.noreply.github.com>
2023-12-04 08:34:18 +0800

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit 2e657fe3eb93e0c69b7ddb0337c4f65ffcd41127, reversing
    changes made to 2950b4650caf907eea47c9cc8834f031a995ef3e.

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+/*
+ * @Author: git config user.name && git config user.email
+ * @Date: 2022-07-17 20:27:06
+ * @LastEditors: git config user.name && git config user.email
+ * @LastEditTime: 2022-10-31 19:23:29
+ * @FilePath: \phi-chart-render\webpack.config.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by ${git_name_email}, All Rights Reserved. 
+ */
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin');
+const GitRevision = new GitRevisionPlugin({ versionCommand: 'describe --always --tags' });
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
+    devtool: 'source-map',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'script-[git-revision-version].js'
+    },
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, 'src')
+        }
+    },
+    devServer: {
+        client: {
+            logging: 'info',
+            progress: true,
+            overlay: {
+                errors: true,
+                warnings: false
+            }
+        },
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+        open: true
+    },
+    plugins: [
+        GitRevision,
+        new webpack.DefinePlugin({
+            GIT_VERSION: JSON.stringify(GitRevision.version())
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public/index.html'),
+            filename: path.join(__dirname, 'dist/index.html'),
+            inject: 'body'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{ loader: 'babel-loader' }]
+            },
+            {
+                test: /\.glsl$/i,
+                use: 'raw-loader'
+            }
+        ]
+    }
+};
